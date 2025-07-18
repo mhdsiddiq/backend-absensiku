c4ef310142c362e81ea93ddb5fd22f86bfb095db
Icha <akhiratunnisahasanah0917@gmail.com>
2025-07-19 01:06:58 +0700

    update

--- config/database.php
+++ config/database.php
@@ -96,11 +96,11 @@ return [
             'driver' => 'mongodb',
             'host' => env('DB_MONGO_HOST', '127.0.0.1'),
             'port' => env('DB_MONGO_PORT', 27017),
-            'database' => env('DB_MONGO_DATABASE', 'absensiku_dashboard'),
+            'database' => env('DB_MONGO_DATABASE', 'absensi_db'),
             'username' => env('DB_MONGO_USERNAME', ''),
             'password' => env('DB_MONGO_PASSWORD', ''),
             'options' => [
-                'database' => env('DB_MONGO_DATABASE', 'absensiku_dashboard'),
+                'database' => env('DB_MONGO_DATABASE', 'absensi_db'),
             ],
         ],
 
